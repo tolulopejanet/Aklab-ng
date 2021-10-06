32b9f73d030a7413cea49723121608fedc21364d
tolulopejanet <tolulopefaleyejanet@gmail.com>
2021-10-06 14:43:40 +0100

    just sorted my navs for small screens

--- js/home.js
+++ js/home.js
@@ -2,18 +2,25 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
 
-  let nav = document.querySelector(".nav");
+  let toggler = document.querySelector('.toggler');
+  let nav = document.querySelector(".nav2");
+  console.log(toggler);
+  let toggleState = 0;
   function showNavMenu() {
-    // console.log(this.children);
-    this.classList.toggle('fa-bars');
+    this.children[0].classList.toggle('bar1');
+    this.children[1].classList.toggle('bar2');
+    this.children[2].classList.toggle('bar3');
 
     if (toggleState == 0){
-        nav.style.display = "none";
+        nav.style.display = "block";
+        document.querySelector(".welcome-text").style.display ="none";
         toggleState += 1;
 
     }
     else{
-        nav.style.display = "block";
+        nav.style.display = "none";
+        document.querySelector(".welcome-text").style.display ="block";
         toggleState -= 1;
     }
 }
+toggler.addEventListener('click', showNavMenu);
